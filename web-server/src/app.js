bd9e20cd7e64d42a305fc8db6c8a0d311abcd99c
Jackson Cruzan <jacksonc@lavi.com>
2020-01-28 15:43:21 -0800

    on session 46

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,9 +1,19 @@
+const path = require("path");
 const express = require("express");
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, "../public")));
+
 app.get("", (req, res) => {
   res.send("Hello Express!");
+});
+
+app.get("/weather", (req, res) => {
+  res.send({
+    forecast: "sunny",
+    location: "location"
+  });
 });
 
 app.listen(3000, () => {
